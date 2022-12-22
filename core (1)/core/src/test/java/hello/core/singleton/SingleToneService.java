31ffa3ce7d3ef00114578d9602bec4b265af7b30
jaeyeon <jaeyeon423@gmail.com>
2022-12-22 19:54:30 +0900

    spring Configuration과 바이트코드 조작의 마법

---
+++ core (1)/core/src/test/java/hello/core/singleton/SingleToneService.java
@@ -0,0 +1,20 @@
+package hello.core.singleton;
+
+public class SingleToneService {
+
+    private static final SingleToneService instance = new SingleToneService();
+
+    public static SingleToneService getInstance(){
+        return instance;
+    }
+
+    private SingleToneService(){
+
+    }
+
+    public void login(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+
+
+}
