5e1b5bd7ebac3e28dc789edf3240274f3e44ddc8
jaeyeon <jaeyeon423@gmail.com>
2022-12-23 21:15:46 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- core (1)/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core (1)/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,17 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
+    private final MemberRepository memberRepository;
+
+    @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
-    private final MemberRepository memberRepository;
 
     @Override
     public void join(Member member) {
