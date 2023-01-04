653b0dbca3898ce821196ce662f20be6f7be86b5
jaeyeon <jaeyeon423@gmail.com>
2023-01-04 18:26:07 +0900

    servelt - HttpServletRequest 기본 사용법

--- servelt/src/main/java/hello/servelt/ServeltApplication.java
+++ servelt/src/main/java/hello/servelt/ServeltApplication.java
@@ -2,7 +2,9 @@ package hello.servelt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //Servlet 자동등록
 @SpringBootApplication
 public class ServeltApplication {
 
