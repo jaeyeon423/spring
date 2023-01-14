a924f2d5fe30674134d1ee8405365af201fda397
jaeyeonkim <jaeyeon423@gmail.com>
2023-01-14 16:39:08 +0900

    실전! 스프링 부트와 JPA 활용1 - 프로젝트 설정 완료

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false) //기본적으로 테스트 후에 작업 RollBack함
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUserName("userA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember = " + findMember); // findMember와 member는 똑같은 녀석이다.
+        System.out.println("member = " + member);
+
+    }
+
+    @Test
+    public void find() {
+    }
+}
