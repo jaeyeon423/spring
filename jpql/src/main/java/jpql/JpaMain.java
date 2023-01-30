a2ee17e5dec4ed74bcec2238db7fb1c2a3bc60f7
jaeyeon <jaeyeon423@gmail.com>
2023-01-30 19:15:03 +0900

    jpql

---
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,28 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.clear();
+        }
+        emf.close();
+    }
+
+}
