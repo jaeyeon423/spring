b23392ddef5defa450e8699fb37a55aa13b1badf
jaeyeon <jaeyeon423@gmail.com>
2023-01-30 12:51:12 +0900

    값 타입

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,20 +15,39 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Address address = new Address("city", "street", "zipcode");
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setHomeAddress(address);
-            em.persist(member1);
+            Member member = new Member();
+            member.setUsername("userA");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
 
-            Member member2 = new Member();
-            member2.setUsername("member1");
-            member2.setHomeAddress(address);
-            em.persist(member2);
 
-            //임베디드 타입 같은 값 타입을 여러 엔티티에서 공유하면 위험
-            member1.getHomeAddress().setCity("newCity");
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
+
+            em.persist(member);
+            
+            em.flush();
+            em.clear();
+
+            System.out.println("===============start==============");
+            Member findMember = em.find(Member.class, member.getId()); //<- collection들은 모두 지연 로딩 LAZY
+
+            //homeCity -> newCity
+//            findMember.getHomeAddress().setCity("newCity"); <- 잘못된 방법 side effect 발생 확률 높음
+
+//            findMember.setHomeAddress(new Address("newCity", "street", "10000")); //<- 새로 값을 넣어줘야한다.
+//
+//            //치킨을 한식으로 변경
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("한식");
+
+            //주소 변경
+//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+//            findMember.getAddressHistory().add(new Address("new1", "street", "10000"));
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,7 +3,9 @@ package hellojpa;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member{
@@ -14,13 +16,19 @@ public class Member{
     @Column(name = "USERNAME")
     private String username;
 
-    //기간 period
-    @Embedded
-    private Period worPeriod;
-
-    //address
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -38,19 +46,27 @@ public class Member{
         this.username = username;
     }
 
-    public Period getWorPeriod() {
-        return worPeriod;
-    }
-
-    public void setWorPeriod(Period worPeriod) {
-        this.worPeriod = worPeriod;
-    }
-
     public Address getHomeAddress() {
         return homeAddress;
     }
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void  setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
