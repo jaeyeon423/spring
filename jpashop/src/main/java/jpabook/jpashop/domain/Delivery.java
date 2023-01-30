ca968474786450e76ff7fa8ee6e7d1b532efe143
jaeyeon <jaeyeon423@gmail.com>
2023-01-30 13:12:40 +0900

    실전 예제 6 - 값 타입으로 변경

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -8,11 +8,34 @@ public class Delivery extends BaseEntity {
     @Id @GeneratedValue
     @Column(name = "DELIVERY_ID")
     private Long id;
-    private String city;
-    private String street;
-    private String zipcode;
-    private DeliveryStatus status;
+    @Embedded
+    private Address address;
 
     @OneToOne(mappedBy = "delivery")
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
