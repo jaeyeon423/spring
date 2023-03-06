b9603d287d3f629662c672bfcfccb786f967b2ac
jaeyeon <jaeyeon423@gmail.com>
2023-03-06 23:00:47 +0900

    스프링 핵심 원리 - 기본편 - 스프링 핵심 원리 이해2 - 새로운 할인 정책 적용과 문제점

---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,41 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 는 10% 할인이 적용되어야 한다.")
+    public void vipTest(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        //when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+    @Test
+    @DisplayName("VIP 가 아니면 10% 할인이 적용되지 말아야 한다..")
+    public void notVipTest(){
+        //given
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+        //when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+
+}
