239f85a28dbe3a67c6caa4cb29d687d6523ffcc7
jaeyeon <jaeyeon423@gmail.com>
2023-03-28 11:01:07 +0900

    트랜잭션 문제 해결 - 트랜잭션 AOP 적용

---
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,54 @@
+package hello.jdbc.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() throws Exception{
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+
+
+    @Test
+    void crud() throws SQLException, InterruptedException {
+        log.info("start");
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        repository.update(member.getMemberId(), 2000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(2000);
+
+        repository.delete(updateMember.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
+
+        Thread.sleep(10);
+    }
+
+}
